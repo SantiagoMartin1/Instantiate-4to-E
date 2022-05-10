3cfd09c8f972b079ef2163ae0d56d02d51f5fe39
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-05-10 14:30:23 -0300

    clase 10/5
    clase 105

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+
+    public Text txtUserName;
+    
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+
+    public void ShowDouble()
+    {
+        if (txtUserName.text != "")
+        {
+            Debug.Log(int.Parse(txtUserName.text) * 2);
+        }
+        else
+        {
+            Debug.LogError("Invalid Input: Empty");
+        }
+        
+    }
+}
